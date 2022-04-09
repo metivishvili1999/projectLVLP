0b59e85ec1d3c867a2a1eb2f021f58b07f728a30
metivishvili1999 <sandro.metivishvili.1@btu.edu.ge>
2022-04-09 07:22:22 +0400

    fixed some bugs

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
+// Function for weekly promotion tabs
+function weeklyPromotions(evt, promotionsName) {
+    var i, promotions, weekTabs;
+    promotions = document.getElementsByClassName("promotions");
+    for (i = 0; i < promotions.length; i++) {
+        promotions[i].style.display = "none";
+    }
+    weekTabs = document.getElementsByClassName("weekTabs");
+    for (i = 0; i < weekTabs.length; i++) {
+        weekTabs[i].className = weekTabs[i].className.replace(" active", "");
+    }
+
+    document.getElementById(promotionsName).style.display = "flex";
+    evt.currentTarget.className += " active";
+}
+
+// Slider Function
 const slider = document.querySelector('.slider-inner');
 const progressBar = document.querySelector('.prog-bar-inner');
 
 let sliderGrabbed = false;
 
 slider.parentElement.addEventListener('scroll', (e) => {
-    progressBar.style.width  = `${getScrollPercentage()}%`
+    progressBar.style.width = `${getScrollPercentage()}%`
 })
 
 slider.addEventListener('mousedown', (e) => {
@@ -22,16 +39,16 @@ slider.addEventListener('mouseleave', (e) => {
 })
 
 slider.addEventListener('mousemove', (e) => {
-    if(sliderGrabbed){
+    if (sliderGrabbed) {
         slider.parentElement.scrollLeft -= e.movementX;
     }
 })
 
-slider.addEventListener('wheel', (e) =>{
+slider.addEventListener('wheel', (e) => {
     e.preventDefault()
     slider.parentElement.scrollLeft += e.deltaY;
 })
 
-function getScrollPercentage(){
-   return ((slider.parentElement.scrollLeft / (slider.parentElement.scrollWidth - slider.parentElement.clientWidth) ) * 100);
+function getScrollPercentage() {
+    return ((slider.parentElement.scrollLeft / (slider.parentElement.scrollWidth - slider.parentElement.clientWidth)) * 100);
 }
