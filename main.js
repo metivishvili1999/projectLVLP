50eb8ea3444d0652ac60ab10b301763efd0396d4
metivishvili1999 <sandro.metivishvili.1@btu.edu.ge>
2022-04-11 04:17:53 +0400

    added responsive

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-// Function for weekly promotion tabs
+// Function for navigation between weekly promotion tabs
 function weeklyPromotions(evt, promotionsName) {
-    var i, promotions, weekTabs;
+    var i, promotions, tabs;
     promotions = document.getElementsByClassName("promotions");
     for (i = 0; i < promotions.length; i++) {
         promotions[i].style.display = "none";
     }
-    weekTabs = document.getElementsByClassName("weekTabs");
-    for (i = 0; i < weekTabs.length; i++) {
-        weekTabs[i].className = weekTabs[i].className.replace(" active", "");
+    tabs = document.getElementsByClassName("tabs");
+    for (i = 0; i < tabs.length; i++) {
+        tabs[i].className = tabs[i].className.replace(" active", "");
     }
 
     document.getElementById(promotionsName).style.display = "flex";
